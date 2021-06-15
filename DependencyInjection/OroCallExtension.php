bc84f098279080833134d1da7f8119765c355b3f
Eugene Chetverikov <EugeneC@users.noreply.github.com>
2021-06-15 13:31:50 +0300

    BAP-20637: Auto-injection of the container is deprecated (#30001)
    - controllers were configured as a services
    - add missed services to the subscribed services list

--- DependencyInjection/OroCallExtension.php
+++ DependencyInjection/OroCallExtension.php
@@ -7,6 +7,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
+/**
+ * This is the class that loads and manages CallBundle service configuration
+ */
 class OroCallExtension extends Extension
 {
     /**
@@ -17,5 +20,6 @@ class OroCallExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('form.yml');
+        $loader->load('controllers.yml');
     }
 }
