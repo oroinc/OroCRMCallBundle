7959ae0d09059f7f929cf921ee16a4343e67664b
Vova Soroka <vsoroka@oroinc.com>
2025-03-15 13:04:23 +0200

    BAP-22968: Replace Doctrine\ORM\EntityManager with EntityManagerInterface in "platform" package (#40751)

--- EventListener/Datagrid/CallListener.php
+++ EventListener/Datagrid/CallListener.php
@@ -2,7 +2,7 @@
 
 namespace Oro\Bundle\CallBundle\EventListener\Datagrid;
 
-use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ManagerRegistry;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use Oro\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource;
 use Oro\Bundle\DataGridBundle\Event\BuildAfter;
@@ -14,14 +14,9 @@ use Oro\Bundle\UserBundle\Entity\User;
  */
 class CallListener
 {
-    /**
-     * @var EntityManager
-     */
-    protected $entityManager;
-
-    public function __construct(EntityManager $entityManager)
-    {
-        $this->entityManager = $entityManager;
+    public function __construct(
+        private ManagerRegistry $doctrine
+    ) {
     }
 
     /**
@@ -42,17 +37,6 @@ class CallListener
     }
 
     /**
-     * @param DatagridConfiguration $config
-     * @param string $fieldName
-     */
-    protected function removeColumn(DatagridConfiguration $config, $fieldName)
-    {
-        $config->offsetUnsetByPath(sprintf('[columns][%s]', $fieldName));
-        $config->offsetUnsetByPath(sprintf('[filters][columns][%s]', $fieldName));
-        $config->offsetUnsetByPath(sprintf('[sorters][columns][%s]', $fieldName));
-    }
-
-    /**
      * Add required filters
      */
     public function onBuildAfter(BuildAfter $event)
@@ -64,7 +48,7 @@ class CallListener
         $parameters = $datagrid->getParameters();
 
         if ($parameters->has('userId')) {
-            $user = $this->entityManager->find(User::class, $parameters->get('userId'));
+            $user = $this->doctrine->getManagerForClass(User::class)->find(User::class, $parameters->get('userId'));
             $queryBuilder
                 ->andWhere('call.owner = :user')
                 ->setParameter('user', $user);
@@ -78,5 +62,12 @@ class CallListener
             $queryBuilder->andWhere($queryBuilder->expr()->in('call.id', ':callIds'))
                 ->setParameter('callIds', $callIds);
         }
+    }
+
+    private function removeColumn(DatagridConfiguration $config, string $fieldName): void
+    {
+        $config->offsetUnsetByPath(sprintf('[columns][%s]', $fieldName));
+        $config->offsetUnsetByPath(sprintf('[filters][columns][%s]', $fieldName));
+        $config->offsetUnsetByPath(sprintf('[sorters][columns][%s]', $fieldName));
     }
 }
